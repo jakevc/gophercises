44f195be220c76ae656405b0d73409c586477c62
Jake VanCampen <jake.vancampen7@gmail.com>
2019-10-15 15:26:55 -0700

    Yaml and Map handlers working

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,17 +2,41 @@ package urlshort
 
 import (
 	"fmt"
+	"github.com/go-yaml/yaml"
 	"net/http"
 )
+
+// struct to parse yaml string
+type T []struct {
+	P string `yaml:"path"`
+	U string `yaml:"url"`
+}
+
+// parse yaml into slice of structs
+func parseYaml(yml []byte) T {
+	t := T{}
+	err := yaml.Unmarshal([]byte(yml), &t)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+	return t
+}
+
+// make map from slice of structs
+func makeMap(yamlStruct T) map[string]string {
+	urlMap := make(map[string]string)
+	for _, s := range yamlStruct {
+		urlMap[s.P] = s.U
+	}
+	return urlMap
+}
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	// handler redirects shortened urls to long ones from a map.
 	return func(w http.ResponseWriter, r *http.Request) {
 		pth := r.URL.Path[0:]
-		fmt.Println(pth)
 		// execute redirect if path is in map
 		if s, ok := pathsToUrls[pth]; ok {
-			fmt.Println(s)
 			http.Redirect(w, r, s, 301)
 		}
 		// fallback to the provided handler
@@ -20,23 +44,9 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+// handle the yaml map using the mapHandler
+func YAMLHandler(yml []byte, fallback http.Handler) http.HandlerFunc {
+	yamS := parseYaml(yml)
+	yamM := makeMap(yamS)
+	return MapHandler(yamM, fallback)
 }
